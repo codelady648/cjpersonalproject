6ee9fac120b46231e966acdd544691653801f454
Doll <rcjcruise@verizon.net>
2016-07-28 18:25:44 +0000

    addition of js file for address input boxes - contactMap page

---
+++ js/myjsfile.js
@@ -0,0 +1,26 @@
+function insert ( ) {
+  var targetInput  = document.getElementById("place1").value;
+var messageBox  = document.getElementById("target1");
+
+messageBox.innerHTML = targetInput;
+ 
+var targetInput2 =  
+  document.getElementById("place2").value;
+var messageBox  = document.getElementById("target2");
+
+messageBox.innerHTML = targetInput2;
+  
+var targetInput3 =  
+  document.getElementById("place3").value;
+var messageBox  = document.getElementById("target3");
+
+messageBox.innerHTML = targetInput3;
+
+var targetInput4 =  
+  document.getElementById("place4").value;
+var messageBox  = document.getElementById("target4");
+
+messageBox.innerHTML = targetInput4;  
+  
+}
+
